80310265ed47f9de294b61d6e9d27d89fd430f5b
jan438 <jan438@users.noreply.github.com>
2024-10-02 11:15:05 +0200

    Albums. pythonadded

---
+++ Albums.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import unicodedata
+from pathlib import Path
+from datetime import datetime, date, timedelta
+from ics import Calendar, Event
+from pypdf import PdfReader, PdfWriter
+from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.pagesizes import LETTER, A4, landscape, portrait
+from reportlab.lib.units import inch
+from reportlab.lib.colors import blue, green, black, red, pink, gray, brown, purple, orange, yellow, white
+from reportlab.pdfbase import pdfmetrics  
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase.pdfmetrics import registerFontFamily
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, Image, Spacer, Frame
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER
+
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Albums'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Albums"
+os.chdir(path)
+styles = getSampleStyleSheet()
+key = input("Wait")
