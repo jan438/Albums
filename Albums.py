2453186b59e643130c91df349e42086056cf53b1
jan438 <jan438@users.noreply.github.com>
2024-11-16 15:14:09 +0100

    nog 2 files

--- Albums.py
+++ Albums.py
@@ -203,6 +203,22 @@ if len(params) > 0:
                 if count > 0:
                     albumdata.append(row)
                 count += 1
+            file_to_open = "Data/Albums25/Albums076-100.csv" 
+        with open(file_to_open, 'r') as file:
+            csvreader = csv.reader(file, delimiter = ';')
+            count = 0
+            for row in csvreader:
+                if count > 0:
+                    albumdata.append(row)
+                count += 1
+            file_to_open = "Data/Albums25/Albums101-125.csv" 
+        with open(file_to_open, 'r') as file:
+            csvreader = csv.reader(file, delimiter = ';')
+            count = 0
+            for row in csvreader:
+                if count > 0:
+                    albumdata.append(row)
+                count += 1
 else:
     file_to_open = "Data/Albums.csv"
     with open(file_to_open, 'r') as file:
