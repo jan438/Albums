f4a32ddbe134e1ead59b1677ef9257a044325e93
jan438 <jan438@users.noreply.github.com>
2024-11-20 10:43:47 +0100

    code adapted

--- Albums.py
+++ Albums.py
@@ -157,8 +157,8 @@ def processcsv(csvfile):
 
 def processdiacritic(text):
     processed = text
-    uumlauts = find_all_occurrences(text, luumlaut, 0, len(text))
-    processed = process_2bytesymbols(text, uumlauts, wuumlaut)
+    uumlauts = find_all_occurrences(processed, luumlaut, 0, len(text))
+    processed = process_2bytesymbols(processed, uumlauts, wuumlaut)
     print("processdiacritic", text, uumlauts)
     return processed
 
