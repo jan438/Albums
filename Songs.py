f6e94d7e194c8b7cfc196c8ed268ef24b103e841
jan438 <jan438@users.noreply.github.com>
2026-04-23 08:57:27 +0200

    song changed

--- Songs.py
+++ Songs.py
@@ -1,2 +1,23 @@
+import os
+import sys
+import csv
+import math
+import unicodedata
+from pathlib import Path
+from datetime import datetime, date, timedelta
+from reportlab.pdfgen import canvas
+from reportlab.lib.colors import HexColor
+from reportlab.lib.units import inch, mm
 
+if sys.platform[0] == 'l':
+    path = '/home/jan/git/Albums'
+if sys.platform[0] == 'w':
+    path = "C:/Users/janbo/OneDrive/Documents/GitHub/Albums"
+os.chdir(path)
+my_canvas = canvas.Canvas("PDF/RSSongs2024.pdf")
+my_canvas.setFillColor(HexColor('#FECDE5'))
+p = my_canvas.beginPath()
+p.arc(2.0, 12.0, 22.0, 32.0, startAng = 90, extent = 90)
+my_canvas.drawPath(p, fill=1, stroke=1)
+my_canvas.save()
 key = input("Wait")
